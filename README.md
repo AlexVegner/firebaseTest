284792ef4f95ed9de367df7430a4e7cfb5b740d9
Alex Vegner <alexvegner@gmail.com>
2019-06-14 15:22:58 +0300

    Update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # firebaseTest
+
+*Prepare Flutter for web web*
+`bash setup.bash`
+
+*Activate webdev*
+`flutter pub global activate webdev`
+> See more info in https://github.com/flutter/flutter_web 
+
+*Init firebase in flutter folder*
+`firebase init`
+
+*Deploy to Firebase*
+`bash deploy.bash`
--- web/lib/main.dart
+++ web/lib/main.dart
@@ -1,9 +1,61 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter_web/material.dart';
 
-void main() {
-  runApp(new Text('Hello World', textDirection: TextDirection.ltr));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Home Page '),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (choose the "Toggle Debug Paint" action
+          // from the Flutter Inspector in Android Studio, or the "Toggle Debug
+          // Paint" command in Visual Studio Code) to see the wireframe for each
+          // widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Flutter web app!',
+            ),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
 }
